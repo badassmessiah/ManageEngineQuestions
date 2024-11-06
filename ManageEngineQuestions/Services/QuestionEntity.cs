af7525763b728e040fc5ce2b80e759075caae6e3
badassmessiah <lashaandguladze69@gmail.com>
2024-11-06 18:13:02 +0400

    Remove ESMInstance class and associated JSON files; update QuestionEntity to include child questions mapping and initialize answer options.

--- ManageEngineQuestions/Services/QuestionEntity.cs
+++ ManageEngineQuestions/Services/QuestionEntity.cs
@@ -10,13 +10,18 @@ namespace ManageEngineQuestions.Services
         public string Explanation { get; set; }
         public QuestionType Type { get; set; }
         public string TextAnswer { get; set; }
-        public List<string> CheckboxAnswers { get; set; }
-        public List<string> DropdownOptions { get; set; }
+        public List<string> DropdownOptions { get; set; } = new();
+        public List<string> CheckboxAnswers { get; set; } = new();
 
+        // Add this property to map answers to child questions
+        public Dictionary<string, List<QuestionEntity>> ChildQuestionsByAnswer { get; set; } = new();
+
+        // Constructor
         public QuestionEntity()
         {
             CheckboxAnswers = new List<string>();
             DropdownOptions = new List<string>();
+            ChildQuestionsByAnswer = new Dictionary<string, List<QuestionEntity>>();
         }
     }
 
@@ -24,7 +29,6 @@ namespace ManageEngineQuestions.Services
     public enum QuestionType
     {
         Text,
-        Checkbox,
         Dropdown
     }
 }
